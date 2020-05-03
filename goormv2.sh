448067ff67e9bc4e5e83e3e71648c6c19f8f335d
小朋 <2810580330@qq.com>
2020-05-03 13:32:39 +0800

    Update goormv2.sh

--- goormv2.sh
+++ goormv2.sh
@@ -14,10 +14,12 @@ wget --no-check-certificate https://github.com/byxiaopeng/goorm-v2ray/raw/master
 chmod +x /v2ray/v2ctl
 rm -f /v2ray/config.json
 wget --no-check-certificate https://github.com/byxiaopeng/goorm-v2ray/raw/master/config.json
-# 开始后台运行
+
 
 #修改系统为北京时间
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 screen -S v2
 /v2ray/v2ray -config=/v2ray/config.json
+# 开始后台运行
+#nohup /v2ray/v2ray -config=/v2ray/config.json >out.txt 2>&1 &
